026781503cd48dc6e3933f549a16c2ae53c55a11
Tomás Rodeghiero <tomirodeghiero@Tomass-MacBook-Air.local>
2023-08-10 19:27:00 -0300

    Add responsive in cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -38,24 +38,24 @@ const CartPage = () => {
         subtitle="Inicio - Carrito de Compras"
       />
 
-      <div className="px-24 py-14 flex w-full gap-8">
-        <div className="w-3/4">
+      <div className="px-4 py-8 lg:px-24 lg:py-14 flex flex-col lg:flex-row gap-8">
+        <div className="w-full lg:w-3/4">
           <div className="overflow-x-auto">
             <table className="w-full border-collapse table-auto">
               <thead className="bg-rose-300 mb-8">
                 <tr>
-                  <th />
-                  <th className="p-3 text-left font-medium">Producto</th>
-                  <th className="p-3 text-left font-medium">Precio</th>
-                  <th className="p-3 text-left font-medium">Cantidad</th>
-                  <th className="p-3 text-left font-medium">Subtotal</th>
+                  <th className="hidden lg:block" />
+                  <th className="p-3 text-center font-medium">Producto</th>
+                  <th className="p-3 text-center font-medium">Precio</th>
+                  <th className="p-3 text-center font-medium">Cantidad</th>
+                  <th className="p-3 text-center font-medium">Subtotal</th>
                   <th />
                 </tr>
               </thead>
               <tbody>
                 {CART_ITEMS.map((item, index) => (
                   <tr key={index}>
-                    <td className="px-3 py-10">
+                    <td className="hidden lg:block lg:px-3 lg:py-10">
                       <Image
                         src={item.image}
                         alt={item.product}
@@ -64,17 +64,19 @@ const CartPage = () => {
                         className="rounded-lg mx-auto"
                       />
                     </td>
-                    <td className="px-3 py-10">{item.product}</td>
-                    <td className="px-3 py-10">{formatPriceARS(item.price)}</td>
-                    <td className="px-3 py-10">
+                    <td className="px-3 py-10 text-center">{item.product}</td>
+                    <td className="px-3 py-10 text-center">
+                      {formatPriceARS(item.price)}
+                    </td>
+                    <td className="px-3 py-10 text-center">
                       <p className="rounded border inline justify-center items-center py-1 px-3 border-gray-500">
                         {item.quantity}
                       </p>
                     </td>
-                    <td className="px-3 py-10">
+                    <td className="px-3 py-10 text-center">
                       {formatPriceARS(item.subtotal)}
                     </td>
-                    <td className="px-3 py-10">
+                    <td className="px-3 py-10 text-center">
                       <Image
                         src={TRASH}
                         alt="Trash"
@@ -90,7 +92,7 @@ const CartPage = () => {
           </div>
         </div>
 
-        <div className="w-1/4 bg-rose-300 pt-5 pb-16 px-10">
+        <div className="w-full lg:w-1/4 bg-rose-300 pt-5 pb-16 px-4 lg:px-10">
           <h1 className="font-semibold text-xl text-center mb-8">
             Total del Carrito
           </h1>
@@ -104,7 +106,7 @@ const CartPage = () => {
             <p className="text-lg text-yellow-600">{formatPriceARS(195)}</p>
           </div>
 
-          <button className="mx-auto flex justify-center rounded-xl border py-[0.7rem] text-center w-3/4 border-black">
+          <button className="mx-auto flex justify-center rounded-xl border py-[0.7rem] text-center w-full border-black">
             Ir a Pagar
           </button>
         </div>
